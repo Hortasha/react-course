d36a419fd4826b7b989f40996378d50e62daf7f6
hortasha <eidemartin_303@hotmail.com>
2019-04-09 15:09:52 +0200

    change favicon and title

--- react/src/index.js
+++ react/src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter as Router, Route } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-//import UpdatedCoursePage from './pages/Course-Updated/Course-Page';
 import CoursePage from './pages/Course-Page/Course-Page';
 
 ReactDOM.render(
